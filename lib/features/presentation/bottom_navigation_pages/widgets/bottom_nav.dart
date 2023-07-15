1da963358360a8825167c41a7116dd966c2f1734
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-07-15 19:39:38 +0100

    feat: imports

--- lib/features/presentation/bottom_navigation_pages/widgets/bottom_nav.dart
+++ lib/features/presentation/bottom_navigation_pages/widgets/bottom_nav.dart
@@ -1,7 +1,7 @@
 export 'package:flutter/material.dart';
 export 'package:rent_house_app/features/presentation/bottom_navigation_pages/widgets/tab_icons.dart';
 export 'package:font_awesome_flutter/font_awesome_flutter.dart';
-export 'package:rent_house_app/features/presentation/chat_messages/chat_message_screen.dart';
+export 'package:rent_house_app/features/presentation/list_chat_messages/list_chat_messages_screen.dart';
 export 'package:rent_house_app/features/presentation/home_screen/home_screen.dart';
 export 'package:rent_house_app/features/presentation/profile/profile_screen.dart';
 export 'package:rent_house_app/features/presentation/filtered_advertisiment/filtered_advertisiment_screen.dart';
