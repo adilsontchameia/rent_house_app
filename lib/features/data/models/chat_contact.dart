12572409f2ff37089aab2fa80ddecfecef04d3ee
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-07-18 19:39:06 +0100

    feat: chat contacts model

--- lib/features/data/models/chat_contact.dart
+++ lib/features/data/models/chat_contact.dart
@@ -1,16 +1,18 @@
-class ChatContacts {
-  final String name;
-  final String profilePic;
-  final String contactId;
-  final DateTime timeSent;
-  final String lastMessage;
+import '../../presentation/filtered_advertisiment/filtered_advertisiment.dart';
 
-  ChatContacts({
-    required this.name,
-    required this.profilePic,
-    required this.contactId,
-    required this.timeSent,
-    required this.lastMessage,
+class ChatContact {
+  final String? name;
+  final String? profilePic;
+  final String? contactId;
+  final DateTime? timeSent;
+  final String? lastMessage;
+
+  ChatContact({
+    this.name,
+    this.profilePic,
+    this.contactId,
+    this.timeSent,
+    this.lastMessage,
   });
 
   Map<String, dynamic> toMap() {
@@ -18,17 +20,20 @@ class ChatContacts {
       'name': name,
       'profilePic': profilePic,
       'contactId': contactId,
-      'timeSent': timeSent.millisecondsSinceEpoch,
+      'timeSent': timeSent != null ? Timestamp.fromDate(timeSent!) : null,
       'lastMessage': lastMessage,
     };
   }
 
-  factory ChatContacts.fromMap(Map<String, dynamic> map) {
-    return ChatContacts(
+  factory ChatContact.fromMap(Map<String, dynamic> map) {
+    Timestamp? timestamp = map['timeSent'] as Timestamp?;
+    DateTime? timeSentDateTime = timestamp?.toDate();
+
+    return ChatContact(
       name: map['name'] as String,
       profilePic: map['profilePic'] as String,
       contactId: map['contactId'] as String,
-      timeSent: DateTime.fromMillisecondsSinceEpoch(map['timeSent'] ?? ''),
+      timeSent: timeSentDateTime,
       lastMessage: map['lastMessage'] as String,
     );
   }
