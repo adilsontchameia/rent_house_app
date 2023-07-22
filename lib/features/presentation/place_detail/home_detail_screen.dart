dbfd6947f623b01cd5659b99405c23019441e672
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-07-22 15:28:06 +0100

    feat: blank screen

---
+++ lib/features/presentation/place_detail/home_detail_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeDetailsScreen extends StatelessWidget {
+  const HomeDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
