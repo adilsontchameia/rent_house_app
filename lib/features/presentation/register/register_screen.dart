6d45ce49215581529db08178f8842b15613f5cc2
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-07-24 08:53:08 +0100

    feat: register new user

--- lib/features/presentation/register/register_screen.dart
+++ lib/features/presentation/register/register_screen.dart
@@ -1,11 +1,16 @@
 import 'dart:async';
-import 'dart:developer';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
+import 'package:rent_house_app/features/presentation/providers/user_provider.dart';
 import 'package:rent_house_app/features/presentation/register/widgets/pin_code_fields.dart';
-import 'package:rent_house_app/features/presentation/widgets/app_logo.dart';
 import 'package:rent_house_app/features/presentation/widgets/custom_input_text.dart';
+import 'package:rent_house_app/features/services/auth_service.dart';
 import 'package:rent_house_app/helpers/validator_mixin.dart';
+
+import 'widgets/location_text_input.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -17,26 +22,45 @@ class RegisterScreen extends StatefulWidget {
 class RegisterScreenState extends State<RegisterScreen> with ValidationMixins {
   //? Global key for validation purpose
   final GlobalKey<FormState> _formKey = GlobalKey();
+  final UserAuthProvider _userAuthProvider = UserAuthProvider();
   //? Controllers
   final TextEditingController firstNameController = TextEditingController();
   final TextEditingController lastNameController = TextEditingController();
-  final TextEditingController phoneNumberController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController currentLocationController =
+      TextEditingController();
   final TextEditingController otpCodeController = TextEditingController();
+  XFile? _image;
 
   Future<void> _submit() async {
-    _showDialogBox();
-    /*
     final validationPassed = _formKey.currentState!.validate();
     final String firstName = firstNameController.text.trim();
     final String lastName = lastNameController.text.trim();
-    final String phoneNumber = phoneNumberController.text.trim();
+    final String email = emailController.text.trim();
+    final String phone = phoneController.text.trim();
     final String password = passwordController.text.trim();
-    final String otpCode = otpCodeController.text.trim();
-    if (validationPassed) {
-      // _userManagerProvider.login(email, password);
+    //final String otpCode = otpCodeController.text.trim();
+    final String address = currentLocationController.text.trim();
+
+    //TODO do not need email will use number and Google
+    if (validationPassed && _image != null) {
+      setState(() {
+        //_showDialogBox();
+        _userAuthProvider.isReady = true;
+        _userAuthProvider.signUp(
+          name: '$firstName $lastName',
+          email: email,
+          phone: phone,
+          image: _image!,
+          address: address,
+          password: password,
+        );
+      });
+    } else {
+      _showDialogBox();
     }
-    */
   }
 
   Future<void> _showDialogBox() async {
@@ -108,14 +132,31 @@ class RegisterScreenState extends State<RegisterScreen> with ValidationMixins {
               ),
             ),
           ),
-          Positioned(
-            top: MediaQuery.of(context).size.height * 0.10,
-            left: 50.0,
-            right: 50.0,
-            child: const AppLogo(),
+          SafeArea(
+            child: Center(
+              child: GestureDetector(
+                onTap: () async {
+                  await _showImageDialogBox();
+                },
+                child: SizedBox(
+                    height: 90.0,
+                    width: 90.0,
+                    child: Card(
+                      color: Colors.white,
+                      elevation: 5,
+                      child: Center(
+                        child: _image == null
+                            ? Image.asset('assets/person.png',
+                                fit: BoxFit.cover)
+                            : Image.file(File(_image!.path),
+                                fit: BoxFit.cover), //After pick image
+                      ),
+                    )),
+              ),
+            ),
           ),
           Positioned(
-            top: MediaQuery.of(context).size.height * 0.30,
+            top: MediaQuery.of(context).size.height * 0.20,
             left: 10.0,
             right: 10.0,
             child: Form(
@@ -145,20 +186,48 @@ class RegisterScreenState extends State<RegisterScreen> with ValidationMixins {
                       validator: (value) => insNotEmpty(value),
                     ),
                     InputText(
+                      icon: Icons.email,
+                      isPassword: false,
+                      keyboardType: TextInputType.name,
+                      controller: emailController,
+                      label: 'Email',
+                      fontSize: 15.0,
+                      validator: (value) => insNotEmpty(value),
+                    ),
+                    InputText(
                       icon: Icons.phone,
                       isPassword: false,
-                      controller: phoneNumberController,
+                      controller: phoneController,
                       keyboardType: TextInputType.emailAddress,
                       label: 'Número de Phone',
                       fontSize: 15.0,
                       validator: (value) => insNotEmpty(value),
                     ),
                     InputText(
-                      icon: Icons.phone,
+                      icon: Icons.password,
                       isPassword: true,
                       controller: passwordController,
                       keyboardType: TextInputType.emailAddress,
                       label: 'Senha',
+                      fontSize: 15.0,
+                      validator: (value) => insNotEmpty(value),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(5.0),
+                      child: Text(
+                        'OBS: Aperte no ícone de gps para buscar sua localização atual.',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12.0,
+                        ),
+                      ),
+                    ),
+                    LocationInputText(
+                      icon: Icons.location_on,
+                      controller: currentLocationController,
+                      keyboardType: TextInputType.emailAddress,
+                      label: 'Sua Localização Atual',
                       fontSize: 15.0,
                       validator: (value) => insNotEmpty(value),
                     ),
@@ -188,7 +257,7 @@ class RegisterScreenState extends State<RegisterScreen> with ValidationMixins {
                       ),
                     ),
                     const Text(
-                      'Ou clicque abaico e registre-se com sua conta Google',
+                      'Ou clicque abaico e entre com sua conta Google',
                       style: TextStyle(
                         fontSize: 13.0,
                         fontWeight: FontWeight.bold,
@@ -197,7 +266,7 @@ class RegisterScreenState extends State<RegisterScreen> with ValidationMixins {
                     ),
                     const SizedBox(height: 5.0),
                     GestureDetector(
-                      onTap: () => log('Google'),
+                      onTap: () => AuthService().signInWithGoogle(),
                       child: Image.asset(
                         'assets/google_icon.png',
                         height: 35.0,
@@ -223,5 +292,86 @@ class RegisterScreenState extends State<RegisterScreen> with ValidationMixins {
         ],
       ),
     ));
+  }
+
+  Future<void> _showImageDialogBox() async {
+    final authData = Provider.of<UserAuthProvider>(context, listen: false);
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.black,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+          title: const Text(
+            'Carregar imagem de perfil.',
+            style: TextStyle(
+              fontSize: 15.0,
+              color: Colors.white,
+            ),
+          ),
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child: ElevatedButton(
+                    onPressed: () => authData.getGalleryImage().then((image) {
+                      setState(() {
+                        _image = image;
+                      });
+                    }),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.white),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                      ),
+                    ),
+                    child: const Text(
+                      'Galeria',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15.0,
+                      ),
+                    ),
+                    //color: Colors.pinkAccent,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 10.0),
+              Expanded(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.white),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5.0),
+                        ),
+                      ),
+                    ),
+                    child: const Text(
+                      'Câmera',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    onPressed: () => authData.getCameraImage().then((image) {
+                      setState(() {
+                        _image = image;
+                      });
+                    }),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
