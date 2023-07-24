52e8e7aaffd5761c59e62560e744f79c1d2df2c2
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-07-24 08:54:25 +0100

    feat: get locations

---
+++ lib/features/services/geo_fire.dart
@@ -0,0 +1,50 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:geoflutterfire2/geoflutterfire2.dart';
+
+class GeofireProvider {
+  CollectionReference? _ref;
+  GeoFlutterFire? _geo;
+
+  GeofireProvider() {
+    _ref = FirebaseFirestore.instance.collection('Locations');
+    _geo = GeoFlutterFire();
+  }
+
+  Stream<List<DocumentSnapshot>> getNearbyDrivers(
+    double lat,
+    double lng,
+    double radius,
+  ) {
+    GeoFirePoint center = _geo!.point(
+      latitude: lat,
+      longitude: lng,
+    );
+    return _geo!
+        .collection(
+            collectionRef: _ref!.where(
+          'status',
+          isEqualTo: 'online',
+        ))
+        .within(
+          center: center,
+          radius: radius,
+          field: 'position',
+        );
+  }
+
+  Stream<DocumentSnapshot> getLocationByIdStream(String id) {
+    return _ref!.doc(id).snapshots(includeMetadataChanges: true);
+  }
+
+  Future<void> create(String id, double lat, double lng) {
+    GeoFirePoint myLocation = _geo!.point(latitude: lat, longitude: lng);
+    return _ref!.doc(id).set({
+      'status': 'online',
+      'position': myLocation.data,
+    });
+  }
+
+  Future<void> delete(String id) {
+    return _ref!.doc(id).delete();
+  }
+}
