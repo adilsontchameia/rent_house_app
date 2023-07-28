656afa7f8f522ac986f0d2467bcf9493ce24a91e
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-07-28 20:13:16 +0100

    feat:added animation

--- lib/features/presentation/filtered_advertisiment/widgets/filtered_card_content.dart
+++ lib/features/presentation/filtered_advertisiment/widgets/filtered_card_content.dart
@@ -12,27 +12,29 @@ class TopPickedCardContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Icon(
-          icon,
-          size: 15.0,
-          color: Colors.white,
-        ),
-        Expanded(
-          child: Text(
-            content,
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 12.0,
-              overflow: TextOverflow.ellipsis,
+    return BounceInDown(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Icon(
+            icon,
+            size: 15.0,
+            color: Colors.white,
+          ),
+          Expanded(
+            child: Text(
+              content,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 12.0,
+                overflow: TextOverflow.ellipsis,
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
