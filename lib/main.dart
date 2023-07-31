9bd47b0930b7a08b45cd9a1bb5ba1f2e00b1e304
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-07-31 21:18:17 +0100

    feat: new provider

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:rent_house_app/app/my_app.dart';
+import 'package:rent_house_app/features/presentation/providers/user_data_provider.dart';
 import 'package:rent_house_app/features/presentation/providers/user_provider.dart';
+import 'package:rent_house_app/features/services/chat_service.dart';
+import 'package:rent_house_app/features/services/user_manager.dart';
 import 'package:rent_house_app/firebase_options.dart';
 
 void main() async {
@@ -17,6 +20,18 @@ void main() async {
       providers: [
         ChangeNotifierProvider<UserAuthProvider>(
           create: (_) => UserAuthProvider(),
+          lazy: false,
+        ),
+        ChangeNotifierProvider<UserManager>(
+          create: (_) => UserManager(),
+          lazy: false,
+        ),
+        ChangeNotifierProvider<UserDataProvider>(
+          create: (_) => UserDataProvider(),
+          lazy: false,
+        ),
+        ChangeNotifierProvider<ChatService>(
+          create: (_) => ChatService(),
           lazy: false,
         ),
       ],
