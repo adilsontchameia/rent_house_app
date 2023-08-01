9d2cc2ff8296398e950036c426bd5a60bb3a2eaa
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-08-01 18:31:27 +0100

    feat: chat message exchange screen

--- lib/features/presentation/chat_messages/chat_messages_screen.dart
+++ lib/features/presentation/chat_messages/chat_messages_screen.dart
@@ -1,7 +1,4 @@
-import 'dart:developer';
-
-import 'package:rent_house_app/features/data/chat_mocked_info.dart';
-
+import 'package:rent_house_app/features/presentation/chat_messages/widgets/bottom_chat_field.dart';
 import 'package:rent_house_app/features/presentation/chat_messages/widgets/chat_list.dart';
 import 'package:rent_house_app/features/services/user_manager.dart';
 
@@ -20,42 +17,30 @@ class ChatMessagesScreen extends StatefulWidget {
   State<ChatMessagesScreen> createState() => _ChatMessagesScreenState();
 }
 
-final TextEditingController _sendMessageController = TextEditingController();
-
 class _ChatMessagesScreenState extends State<ChatMessagesScreen> {
-  
-
-  @override
-  void dispose() {
-    try {
-      _sendMessageController.dispose();
-    } catch (e) {
-      log(e.toString());
-    }
-    super.dispose();
-  }
-
-  bool isWritting = false;
-
   final UserManager _userManager = UserManager();
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
-    return StreamBuilder(
-        stream: _userManager.getByIdAsStream( widget.uid;),
-        builder: (context, snapshot) {
-          return Scaffold(
-            backgroundColor: const Color.fromARGB(255, 212, 212, 212),
-            appBar: AppBar(
-              backgroundColor: Colors.white,
-              iconTheme: const IconThemeData(color: Colors.brown),
-              title: Row(
+
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 212, 212, 212),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        iconTheme: const IconThemeData(color: Colors.brown),
+        title: StreamBuilder(
+            stream: _userManager.getSellerById(widget.uid),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              return Row(
                 children: [
                   ClipRRect(
                     borderRadius: BorderRadius.circular(80.0),
                     child: CachedNetworkImage(
-                      imageUrl: info[3]['profilePic'].toString(),
+                      imageUrl: snapshot.data!.image!,
                       fit: BoxFit.fill,
                       height: 30.0,
                       width: 30.0,
@@ -71,91 +56,40 @@ class _ChatMessagesScreenState extends State<ChatMessagesScreen> {
                     ),
                   ),
                   const SizedBox(width: 10.0),
-                  Text(
-                    widget.name,
-                    style: const TextStyle(
-                      color: Colors.brown,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18.0,
-                    ),
-                  ),
-                ],
-              ),
-              centerTitle: false,
-            ),
-            body: Column(
-              children: [
-                const Expanded(
-                  child: ChatList(),
-                ),
-                TextFormField(
-                  style: const TextStyle(color: Colors.black),
-                  cursorColor: Colors.black,
-                  onChanged: (value) {
-                    setState(() {
-                      if (value.trim().isNotEmpty) {
-                        isWritting = true;
-                      } else {
-                        isWritting = false;
-                      }
-                    });
-                  },
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white.withOpacity(0.5),
-                    focusColor: Colors.transparent,
-                    enabledBorder: const OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20.0),
-                        topRight: Radius.circular(20.0),
-                      ),
-                    ),
-                    focusedBorder: const OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20.0),
-                        topRight: Radius.circular(20.0),
-                      ),
-                    ),
-                    suffixIcon: Visibility(
-                      visible: isWritting,
-                      child: Container(
-                        height: 20.0,
-                        width: 20.0,
-                        margin: const EdgeInsets.all(5),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20)),
-                        child: IconButton(
-                          onPressed: () {
-                            setState(() {
-                              _sendMessageController.text = '';
-                              log('Message Sent');
-                            });
-                          },
-                          icon: const Icon(
-                            FontAwesomeIcons.paperPlane,
-                            size: 18.0,
-                          ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.name,
+                        style: const TextStyle(
                           color: Colors.brown,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
                         ),
                       ),
-                    ),
-                    hintText: 'Escreva um mensagem aqui...',
-                    hintStyle: const TextStyle(color: Colors.black),
-                    border: const OutlineInputBorder(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(10.0),
-                        topRight: Radius.circular(10.0),
+                      Text(
+                        snapshot.data!.isOnline! ? 'online' : 'offline',
+                        style: const TextStyle(
+                          color: Colors.black38,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 13.0,
+                        ),
                       ),
-                    ),
-                    contentPadding: const EdgeInsets.all(15),
-                  ),
-                ),
-              ],
-            ),
-          );
-        });
+                    ],
+                  )
+                ],
+              );
+            }),
+        centerTitle: false,
+      ),
+      body: Column(
+        children: [
+          const Expanded(
+            child: ChatList(),
+          ),
+          BottomChatField(receiverId: widget.uid),
+        ],
+      ),
+    );
   }
 }
