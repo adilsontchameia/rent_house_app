a507b0f85613449182d391dec6d7f5d7b8f93b4a
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-08-01 18:30:44 +0100

    feat: routing settings

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:rent_house_app/features/presentation/home/home.dart';
-import 'package:rent_house_app/helpers/factories/dialog_factory.dart';
+import 'package:rent_house_app/features/presentation/check_auth/check_auth_screen.dart';
+
+import 'app.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,10 +9,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       navigatorKey: DialogFactory.navigatorKey,
-      home: MaterialApp(
-        theme: ThemeData(useMaterial3: true),
-        home: const HomeScreen(),
-      ),
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: (settings) => generateRoute(settings),
+      theme: ThemeData(useMaterial3: true),
+      home: const CheckAuthScreen(),
     );
   }
 }
