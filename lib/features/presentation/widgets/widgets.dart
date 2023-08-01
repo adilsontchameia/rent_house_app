bce0cfc4a1fe9079fe470b2ec62bf9363d19e706
Adilson Tchameia <adilsonkchameia@outlook.com>
2023-08-01 18:28:05 +0100

    feat: new imports

---
+++ lib/features/presentation/widgets/widgets.dart
@@ -0,0 +1,4 @@
+export 'package:flutter/material.dart';
+
+export 'package:animate_do/animate_do.dart';
+export 'package:font_awesome_flutter/font_awesome_flutter.dart';
---
+++ lib/features/services/services.dart
@@ -0,0 +1,17 @@
+export 'package:cloud_firestore/cloud_firestore.dart';
+export 'package:rent_house_app/features/data/models/advertisement_model.dart';
+export 'package:rent_house_app/features/data/models/user_model.dart';
+export 'dart:developer';
+
+export 'package:firebase_auth/firebase_auth.dart';
+export 'package:get/route_manager.dart';
+export 'package:google_sign_in/google_sign_in.dart';
+export 'package:rent_house_app/features/presentation/bottom_navigation_pages/bottom_navigation_screen.dart';
+export 'package:rent_house_app/features/presentation/login/login_screen.dart';
+export 'package:geoflutterfire2/geoflutterfire2.dart';
+export 'package:rent_house_app/features/data/models/seller_model.dart';
+
+export 'dart:io';
+
+export 'package:firebase_storage/firebase_storage.dart';
+export 'package:image_picker/image_picker.dart';
